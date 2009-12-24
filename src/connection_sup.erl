69fd9bc6bbe3da1a1c175c177436a247dfff2338
mijkenator <mijkenator@gmail.com>
2009-12-24 18:13:26 +0300

    fixs

--- src/connection_sup.erl
+++ src/connection_sup.erl
@@ -10,9 +10,9 @@
 -export([start_link/2, init/1]).
 -export([start_client/2]).
 
-start_client(Name, Module) ->
-    io:format("conn SUP: ~p ~p ~n ", [Name, Module]),
-    supervisor:start_child(connection_sup, [{Module, Name}]).
+start_client(Module, Name) ->
+    supervisor:start_child(connection_sup,
+        {Name, {Module, start_link, [Name]}, temporary, 2000, worker, [Module]}).
 
 start_link(MaxWorkers, Module) ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, [MaxWorkers, Module]).
@@ -31,19 +31,5 @@ init([MaxWorkers, Module]) ->
               }
             ]
         }
-    };
-init([{Module, Name}]) ->
-    {ok,
-        {{simple_one_for_one, 1, 60},
-            [
-              % worker
-              {   undefined,                         % Id       = internal id
-                  {Module, start_link, [Name]},          % StartFun = {M, F, A}
-                  temporary,                         % Restart  = permanent | transient | temporary
-                  2000,                              % Shutdown = brutal_kill | int() >= 0 | infinity
-                  worker,                            % Type     = worker | supervisor
-                  []                                 % Modules  = [Module] | dynamic
-              }
-            ]
-        }
-    }.    
+    }.
+
