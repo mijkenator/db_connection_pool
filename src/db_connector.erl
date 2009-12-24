13aa9b6bc6013f29d8de7e03bb29b5b895f125bf
mijkenator <mijkenator@gmail.com>
2009-12-24 18:55:04 +0300

    fixs

--- src/db_connector.erl
+++ src/db_connector.erl
@@ -7,16 +7,11 @@
 -author('mijkenator@gmail.com').
 
 -export([start_link/1]).
--export([init/1]).
+-export([init/1, code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 
-%% FSM States
--export([
-    'WAIT_FOR_JOB'/2,
-    'WAIT_FOR_WORK'/2
-]).
 -compile(export_all).
 
--behaviour(gen_fsm).
+-behaviour(gen_server).
 
 -record(connector_state,
     {   workername,
@@ -26,38 +21,22 @@
         counter=0}).
         
 start_link(WorkerName) ->
-    io:format("db connector started ~p ~n", [WorkerName]),
-    Result = gen_fsm:start_link({local, WorkerName},?MODULE, #connector_state{workername = WorkerName, counter=0}, []),
-    io:format("db connector started Result ~p ~p ~n", [Result, WorkerName]),
-    Result;
-start_link([M]) -> io:format("db connector started 11111 ~p ~n", [M]).
+    gen_server:start_link({local, WorkerName},?MODULE,
+        #connector_state{workername = WorkerName, counter=0}, []).
 
 init(Args) ->
   io:format("db connector init callback launched ~p ~n", [Args]),
-  {ok, 'WAIT_FOR_JOB', Args, 10}.
+  {ok, Args}.
 
-'WAIT_FOR_JOB'(_Other, State) ->
-    io:format("wait for job ~p ~n", [State]),
-    {next_state, 'WAIT_FOR_JOB', State, 5000}.
-    
-'WAIT_FOR_WORK'(_Other, State) ->
-    io:format("wait for work ~p ~n", [State]),
-    {next_state, 'WAIT_FOR_WORK', State, 5000}.
+handle_cast(Msg, State) ->
+    io:format("db connector unknown cast !!!! ~p ~p ~n", [Msg, State]),
+    {noreply, State}.
 
-
-handle_event(Event, StateName, StateData) ->
-    {stop, {StateName, undefined_event, Event}, StateData}.
+handle_call(_Msg, _Caller, State) ->
+    io:format("db connector unknown call !!!! ~p ~p ~p ~n", [_Msg, _Caller, State]),
+    {noreply, State}.
     
-handle_sync_event(Event, _From, StateName, StateData) ->
-    {stop, {StateName, undefined_event, Event}, StateData}.
-    
-code_change(_OldVsn, StateName, StateData, _Extra) ->
-    {ok, StateName, StateData}.
-    
-handle_info(Info, StateName, StateData) ->
-    io:format("nexpected message ~p ~p ~p ~n", [Info, StateName, StateData]),
-    {stop, normal, StateData}.
-    
-terminate(_Reason, _StateName, _State) ->
-    io:format("Terminate ~n"),
-    ok.
+% These are just here to suppress warnings.    
+handle_info(_Msg, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVersion, State, _Extra) -> {ok, State}.
